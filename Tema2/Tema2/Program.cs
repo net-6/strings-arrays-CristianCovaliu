4d375dd9cef37248f93b9ea69e170dcd4547db41
Cristian Covaliu <cristian.covaliu@gmail.com>
2019-12-04 20:05:54 +0200

    first commit

---
+++ Tema2/Tema2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
